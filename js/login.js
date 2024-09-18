52406b0d0b519f39a3b4b0cf993132aaaf4f3fab
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-09-18 19:57:50 +0600

    login page done

---
+++ js/login.js
@@ -0,0 +1,15 @@
+// console.log("Hello from login.js");
+document.getElementById("login-button").addEventListener("click",function(event){
+    event.preventDefault(); // Prevent automatically reload
+    console.log("Login button clicked");
+    const phoneNumber = document.getElementById("phone-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    if(phoneNumber !=='01994494143' || pinNumber !=='1234'){
+        alert("Invalid phone number or pin number");
+        return;
+    }
+    else{
+        console.log("Login successful");
+        window.location.href = "home.html";
+    }
+})
